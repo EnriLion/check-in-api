00368c4776731b99070d0ac0e9bbc3b2c0c9653c
EnriLion <enriqueleon0201leon@gmail.com>
2024-10-11 11:21:59 -0600

    Creating endpoints

--- check-in-check-out-system/src/main/java/com/tcs/check_in_check_out_system/service/CheckInService.java
+++ check-in-check-out-system/src/main/java/com/tcs/check_in_check_out_system/service/CheckInService.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class CheckInService {
@@ -16,12 +19,30 @@ public class CheckInService {
     private CheckInRepository checkInRepository;
 
 
-    public CheckInModel createdCheckIn(Long checkInId){
+    public CheckInModel createdCheckIn(){
         CheckInModel checkInModel = new CheckInModel();
         checkInModel.setCheckInTime(LocalDateTime.now());
         checkInModel.setCheckOutTime(LocalDateTime.now());
-        checkInModel.setStatus(true);
-        return checkInRepository.save(checkInModel);
+        checkInModel.setStatus(false);
+        checkInRepository.save(checkInModel);
+        return checkInModel;
+    }
+
+    public void deleteRecord(Long checkInId){
+        checkInRepository.deleteById(checkInId);
+    }
+
+    public List<CheckInModel> getCheck(){
+       List<CheckInModel>  check = new ArrayList<>();
+       for(CheckInModel checkInModel : checkInRepository.findAll())
+           check.add(checkInModel);
+       return check;
+    }
+
+    public CheckInModel updateStatus(Long checkInId, boolean status){
+       CheckInModel checkInModel = checkInRepository.findById(checkInId).orElseThrow(NoSuchElementException:: new);
+       checkInModel.setStatus(status);
+       return  checkInRepository.save(checkInModel);
     }
 
 }
